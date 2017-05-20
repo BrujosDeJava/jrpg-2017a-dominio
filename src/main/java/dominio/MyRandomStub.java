a5655b196ae32a673be997967e412193db3a1b75
SilvaDiegoUnlam <spookycynicism@gmail.com>
2017-05-20 01:06:30 -0300

    agregado MyRandomStub

---
+++ src/main/java/dominio/MyRandomStub.java
@@ -0,0 +1,11 @@
+package dominio;
+
+public class MyRandomStub extends RandomNumberGenerator{
+	
+	public static double randomDouble(double n) {
+		return n;
+	}
+	public static int randomInt(int n) {
+		 return n;
+	}
+}
