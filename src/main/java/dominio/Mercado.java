119d9cb5a463076b80e83054cfde8a2ddfe7db6c
SilvaDiegoUnlam <spookycynicism@gmail.com>
2017-07-05 20:18:21 -0300

    preimplementacion de mercado

--- src/main/java/dominio/Mercado.java
+++ src/main/java/dominio/Mercado.java
@@ -1,13 +1,21 @@
 package dominio;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Mercado {
-	private Map<Integer,Inventario> mochilas;
+	private Map<Integer,ArrayList<Item>> mochilas;
 	
 	public Mercado(){
-		this.mochilas = new HashMap<Integer,Inventario>();
-		
+		this.mochilas = new HashMap<Integer,ArrayList<Item>>();
+	}
+	
+	public Map<Integer, ArrayList<Item>> getMochilas() {
+		return mochilas;
+	}
+	
+	public void setMochilas(Map<Integer, ArrayList<Item>> mochilas) {
+		this.mochilas = mochilas;
 	}
 }
